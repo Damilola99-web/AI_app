b71eaaef874828c5ce6539f95986a647528b9e02
Damilola99-web <rasheedw622@gmail.com>
2023-09-25 14:35:45 +0100

    dashboard done

--- components/custom/Sidebar.tsx
+++ components/custom/Sidebar.tsx
@@ -1,14 +1,25 @@
 'use client';
 
+// import { routes } from '@/lib/helper';
 import { cn } from '@/lib/utils';
-import { Code, ImageIcon, LayoutDashboard, MessagesSquare, Music, Settings, VideoIcon } from 'lucide-react';
 import { Montserrat } from 'next/font/google';
+import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const montserrat = Montserrat({ weight: '600', subsets: ['latin'] });
 
-const routes = [
+import {
+	Code,
+	ImageIcon,
+	LayoutDashboard,
+	MessagesSquare,
+	Music,
+	Settings,
+	VideoIcon,
+} from 'lucide-react';
+
+export const routes = [
 	{
 		label: 'Dashboard',
 		icon: LayoutDashboard,
@@ -53,6 +64,7 @@ const routes = [
 ];
 
 const Sidebar = () => {
+	const pathname = usePathname();
 	return (
 		<div className=' space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white'>
 			<div className='px-3 py-2 flex-1'>
@@ -79,7 +91,12 @@ const Sidebar = () => {
 				<div className='space-y-1'>
 					{routes.map((route) => (
 						<Link
-							className='text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/70 rounded-lg transition'
+							className={cn(
+								'text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition',
+								pathname === route.href
+									? 'text-white bg-white/10'
+									: 'text-zinc-400'
+							)}
 							key={route.href}
 							href={route.href}
 						>
@@ -87,6 +104,7 @@ const Sidebar = () => {
 								<route.icon
 									className={cn('h-5 w-5 mr-3', route.color)}
 								></route.icon>
+								{route.label}
 							</div>
 						</Link>
 					))}
